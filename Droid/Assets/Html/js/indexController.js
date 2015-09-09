a0b4e6bbf7c170a3dbdc81b21ef5786d33af90a9
xasan32sds <da@sdsff.drf>
2015-09-09 14:29:21 +0500

    added grid for stolen and expenses

--- Droid/Assets/Html/js/indexController.js
+++ Droid/Assets/Html/js/indexController.js
@@ -1,15 +1,22 @@
-angular.module('clientApp').controller('indexController', ['$scope', function ($scope)
-{
-    
+angular.module('clientApp').controller('indexController', ['$scope', function ($scope) {
+
+    $scope.user = {};
+    $scope.user.ExpenseItems = [];
+    $scope.user.StolenItems = [];
+
+    $scope.NewStolenItems = function () {
+        $scope.user.StolenItems.push({ Name: "", Ammount: "" });
+    };
+    $scope.NewStolenItems();
+
+    $scope.NewExpenseItem = function () {
+        $scope.user.ExpenseItems.push({ Name: "", Ammount: "" });
+    };
+    $scope.NewExpenseItem();
+
     $scope.GetUser = function () {
         return JSON.stringify($scope.user);
-    };
-
-    //$scope.SubmitForm = function (user)
-    //{
-    //    alert(JSON.stringify(user));
-    //};
-    
+    };    
 
 } ]);
 
